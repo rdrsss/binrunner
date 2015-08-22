5f442b12d97d3cb367825dd6d866ba784d7a0dc4
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2015-08-22 00:07:53 -0600

    stub in binrunner

---
+++ src/binrunner.go
@@ -0,0 +1,11 @@
+/*
+ * @file : processmanager.go
+ * @brief : binrunner, start, kill, list, processes via http
+ *			interface.
+ * @author : Manuel A. Rodriguez (manuel.rdrs@gmail.com)
+ */
+package main
+
+func main() {
+
+}
